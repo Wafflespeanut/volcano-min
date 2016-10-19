5f984715bd4afa50373bd237b2f3214253d56203
Ravi Shankar <wafflespeanut@gmail.com>
2016-10-19 19:28:25 +0530

    Dashed (infinite) lines for boundaries

--- volcano.py
+++ volcano.py
@@ -2,7 +2,10 @@ import json
 import matplotlib.pyplot as plt
 
 OFFSET = 0.05       # offset for axis limits
-WIDTH_IN, HEIGHT_IN = 10, 7     # matplotlib doesn't allow resolution (using inches)
+# matplotlib doesn't allow setting image resolution (however, it offers setting the size in inches)
+WIDTH_IN, HEIGHT_IN = 10, 7
+DASHES = [5, 5]     # 8 dots for dash, 6 dots for space
+COLOR = 'blue'
 
 # Draw four lines (and form a box). All points inside this region will be
 # (scatter-)plotted, whereas those outside will be handed over to HTML
@@ -57,7 +60,7 @@ fig, ax = plt.subplots()
 # Initial scatter plot
 ax.set_xlim([min_x - OFFSET, max_x + OFFSET])
 ax.set_ylim([min_y - OFFSET, max_y + OFFSET])
-ax.scatter(x, y)
+ax.scatter(x, y, c=COLOR)
 
 fig.set_size_inches(WIDTH_IN, HEIGHT_IN)
 
@@ -67,7 +70,18 @@ x_1 = (max_x + RECT_OFFSET if RECT_X_MAX is None else RECT_X_MAX)
 y_0 = (min_y - RECT_OFFSET) if RECT_Y_MIN is None else RECT_Y_MIN
 y_1 = (max_y + RECT_OFFSET) if RECT_Y_MAX is None else RECT_Y_MAX
 
-ax.plot((x_0, x_0, x_1, x_1, x_0), (y_0, y_1, y_1, y_0, y_0))
+ax.plot((x_0, x_0, x_1, x_1, x_0), (y_0, y_1, y_1, y_0, y_0), c=COLOR)
+
+# dashed lines
+off = RECT_OFFSET + OFFSET      # just to keep it farther
+ax.plot((x_0, x_0), (min_y - off, y_0), dashes=DASHES, c=COLOR)
+ax.plot((x_0, x_0), (y_0, max_y + off), dashes=DASHES, c=COLOR)
+ax.plot((x_1, x_1), (y_1, max_y + off), dashes=DASHES, c=COLOR)
+ax.plot((x_1, x_1), (y_0, min_y - off), dashes=DASHES, c=COLOR)
+ax.plot((min_x - off, x_0), (y_0, y_0), dashes=DASHES, c=COLOR)
+ax.plot((min_x - off, x_0), (y_1, y_1), dashes=DASHES, c=COLOR)
+ax.plot((x_1, max_x + off), (y_1, y_1), dashes=DASHES, c=COLOR)
+ax.plot((x_1, max_x + off), (y_0, y_0), dashes=DASHES, c=COLOR)
 
 width, height = fig.canvas.get_width_height()
 
@@ -77,7 +91,7 @@ for pts in zip(x_neg, y_neg):
     obj['x'].append(x_)
     obj['y'].append(height - y_)    # invert the height (since it's calculated from top-left in HTML)
 
-fig.savefig('plot.png', dpi=fig.dpi)
+fig.savefig('plot.png', dpi=fig.dpi)    # should be the same as the figure's dpi
 
 with open('gen.js', 'w') as fd:
     data = 'var obj = %s;' % json.dumps(obj, indent=2)
