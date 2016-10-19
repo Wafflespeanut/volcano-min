e3030bee04416d12b38983ac13d5aafce3467e6f
Ravi Shankar <wafflespeanut@gmail.com>
2016-10-19 16:49:59 +0530

    Isolate points into rectangle (and generate JS file)

--- volcano.py
+++ volcano.py
@@ -1,27 +1,84 @@
+import json
 import matplotlib.pyplot as plt
 
-OFFSET = 0.05   # offset for axis limits
-Y_UPPER = 2.0   # upper limit for Y (up to which we're planning to hard-code)
+OFFSET = 0.05       # offset for axis limits
+WIDTH_IN, HEIGHT_IN = 10, 7     # matplotlib doesn't allow resolution (using inches)
 
+# Draw four lines (and form a box). All points inside this region will be
+# (scatter-)plotted, whereas those outside will be handed over to HTML
+RECT_X_MIN = None
+RECT_Y_MIN = None
+RECT_X_MAX = None
+RECT_Y_MAX = 1.5
+RECT_OFFSET = 2 * OFFSET    # so that the edges lying over the axes are out of the picture
+
+x, y = [], []
+x_neg, y_neg = [], []
+obj = {
+    'names': [],
+    'x': [],
+    'y': [],
+}
+
+# CSV format: name,x,y
 with open('voldat.csv', 'r') as fd:
-    data = map(lambda line: line.split(',')[1:3], fd.readlines())[1:]
+    next(fd)
+    min_x, min_y, max_x, max_y = float('inf'), float('inf'), float('-inf'), float('-inf')
 
-x, y = zip(*map(lambda (x, y): (float(x), float(y)), data))
-min_x, max_x, min_y, max_y = min(x), max(x), min(y), max(y)
+    for line in fd:
+        data = iter(line.split(','))
+        name = next(data)
+        x_ = float(next(data))
+        y_ = float(next(data))
+
+        if (RECT_X_MIN is not None and x_ < RECT_X_MIN) or \
+           (RECT_X_MAX is not None and x_ > RECT_X_MAX) or \
+           (RECT_Y_MIN is not None and y_ < RECT_Y_MIN) or \
+           (RECT_Y_MAX is not None and y_ > RECT_Y_MAX):
+            obj['names'].append(name)
+            x_neg.append(x_)
+            y_neg.append(y_)
+
+        x.append(x_)
+        y.append(y_)
+
+        if x_ < min_x:
+            min_x = x_
+        if x_ > max_x:
+            max_x = x_
+        if y_ < min_y:
+            min_y = y_
+        if y_ > max_y:
+            max_y = y_
+
 
 fig, ax = plt.subplots()
 
+# Initial scatter plot
 ax.set_xlim([min_x - OFFSET, max_x + OFFSET])
-ax.set_ylim([min_y, max_y + OFFSET])
+ax.set_ylim([min_y - OFFSET, max_y + OFFSET])
 ax.scatter(x, y)
 
-fig.set_size_inches(10, 7)      # no way to set 'resolution' for the image
-ax.plot((min_x - OFFSET, max_x + OFFSET), (Y_UPPER, Y_UPPER))
+fig.set_size_inches(WIDTH_IN, HEIGHT_IN)
 
-min_x_pos = x.index(min_x)          # sample point (we choose the minimum for now)
-pts = (x[min_x_pos], y[min_x_pos])
-pts_disp = ax.transData.transform(pts)          # get the pixels corresponding to the point
+# form the rectangle
+x_0 = (min_x - RECT_OFFSET) if RECT_X_MIN is None else RECT_X_MIN
+x_1 = (max_x + RECT_OFFSET if RECT_X_MAX is None else RECT_X_MAX)
+y_0 = (min_y - RECT_OFFSET) if RECT_Y_MIN is None else RECT_Y_MIN
+y_1 = (max_y + RECT_OFFSET) if RECT_Y_MAX is None else RECT_Y_MAX
+
+ax.plot((x_0, x_0, x_1, x_1, x_0), (y_0, y_1, y_1, y_0, y_0))
+
 width, height = fig.canvas.get_width_height()
-print pts, pts_disp[0], height - pts_disp[1]    # since we'll need the height from top
+
+# get the pixels corresponding to the collected points
+for pts in zip(x_neg, y_neg):
+    x_, y_ = ax.transData.transform(pts)
+    obj['x'].append(x_)
+    obj['y'].append(height - y_)    # invert the height (since it's calculated from top-left in HTML)
 
 fig.savefig('plot.png', dpi=fig.dpi)
+
+with open('gen.js', 'w') as fd:
+    data = 'var obj = %s;' % json.dumps(obj, indent=2)
+    fd.write(data)
